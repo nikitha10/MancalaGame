faa6c5341bab1c513155ac8d3c284b33fa134eb9
Ally <junkpineapples23@gmail.com>
2019-06-11 13:07:18 -0400

    hey guys

---
+++ Player.java
@@ -0,0 +1,17 @@
+
+/**
+ * Abstract class Player - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Player
+{
+    private int x;
+    
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
