cedca126785756f55bd3ba0ba82aa1fe775e9f24
Nikitha Subramaniam <s603091@ewrsd.k12.nj.us>
2019-06-11 12:57:22 -0400

    game class

---
+++ Game.java
@@ -0,0 +1,48 @@
+
+public class Game
+{
+    private int numStones, stonesGoal1, stonesGoal2;
+    private Integer[] board = new Integer[14];
+
+    public boolean gameIsOver()
+    {
+        int eachSpace, sumOfSpaces = 0;
+
+        for (int i = 0; i < board.length; i++)
+        {
+            if (!(((i == 0) || (i == 13))))
+            {
+                eachSpace = board[i];
+                sumOfSpaces += eachSpace;
+            }
+        }
+
+        if ((sumOfSpaces == 0) 
+        || (stonesGoal1 > sumOfSpaces + stonesGoal2)
+        || (stonesGoal2 > sumOfSpaces + stonesGoal1))
+            return true;
+
+        return false;
+    }
+
+    public int determineWinner()
+    {
+        if (gameIsOver())
+        {
+            if (stonesGoal1 > stonesGoal2)
+            { 
+                System.out.println("Player 1 wins!");
+                return 1;
+            }
+
+            else if (stonesGoal2> stonesGoal1)
+            {
+                System.out.println ("Player 2 wins!");
+                return 2; 
+            }
+            else
+                System.out.println("It’s a tie!");
+        }
+        return 0;
+    }
+}
